94f9bc3b1e1383ee908c3d3651785edb44e292e8
zavr-1 <zavr-1@users.noreply.github.com>
2020-03-01 21:56:29 +0300

    Initial commit

---
+++ splendid/elements/pages.jsx
@@ -0,0 +1,32 @@
+/**
+ * @param {Object} props
+ * @param {Splendid} props.splendid
+ * @param {string} [dir] Pages from this directory only.
+ * @param {boolean} [indexes] Print links that are indexes of dirs only.
+ */
+export default function Pages({ splendid, dir, indexes }) {
+  const { pages, page: { key } } = splendid
+  const menuPages = pages.filter(({ dir: d, index }) => {
+    if (indexes) {
+      return d && index
+    }
+    if (dir) return d == dir
+    if (!d) return true
+  })
+  const ajax = (<ul>
+    {menuPages.map(({
+      title, menu = title, url, menuUrl = url, file, key: k,
+    }) => {
+      const active = k == key
+      return (<li className={active ? 'Active' : ''}>
+        <a data-file={file} href={menuUrl}>{menu}</a>
+      </li>)
+    }
+    )}
+  </ul>)
+  return ajax
+}
+
+/**
+ * @typedef {import('splendid')} Splendid
+ */
